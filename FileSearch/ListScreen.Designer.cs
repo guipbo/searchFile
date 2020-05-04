5d125d500abca59aa10400cf601d0d0e7204e000
Guilherme Patrick Braga de Oliveira <42107033+guipbo@users.noreply.github.com>
2020-05-04 20:36:23 -0300

    Add files via upload

---
+++ FileSearch/ListScreen.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FileSearch
+{
+    partial class ListScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_SearchFile = new System.Windows.Forms.Button();
+            this.Txt_FileName = new System.Windows.Forms.TextBox();
+            this.Lb_File = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Btn_SearchFile
+            // 
+            this.Btn_SearchFile.Location = new System.Drawing.Point(533, 27);
+            this.Btn_SearchFile.Name = "Btn_SearchFile";
+            this.Btn_SearchFile.Size = new System.Drawing.Size(67, 20);
+            this.Btn_SearchFile.TabIndex = 0;
+            this.Btn_SearchFile.Text = "Buscar";
+            this.Btn_SearchFile.UseVisualStyleBackColor = true;
+            this.Btn_SearchFile.Click += new System.EventHandler(this.btn_SearchButton_Click);
+            // 
+            // Txt_FileName
+            // 
+            this.Txt_FileName.Location = new System.Drawing.Point(12, 27);
+            this.Txt_FileName.Name = "Txt_FileName";
+            this.Txt_FileName.Size = new System.Drawing.Size(515, 20);
+            this.Txt_FileName.TabIndex = 0;
+            this.Txt_FileName.ReadOnly = true;
+            this.Txt_FileName.Click += new System.EventHandler(this.txt_FileName_Click);
+            // 
+            // Lb_File
+            // 
+            this.Lb_File.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lb_File.Location = new System.Drawing.Point(12, 9);
+            this.Lb_File.Name = "Lb_File";
+            this.Lb_File.Size = new System.Drawing.Size(51, 15);
+            this.Lb_File.TabIndex = 0;
+            this.Lb_File.Text = "Arquivo";
+            // 
+            // ListScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 59);
+            this.Controls.Add(this.Btn_SearchFile);
+            this.Controls.Add(this.Txt_FileName);
+            this.Controls.Add(this.Lb_File);
+            this.Name = "ListScreen";
+            this.Text = "Abrir arquivo";
+            this.Load += new System.EventHandler(this.ListScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_SearchFile;
+        private System.Windows.Forms.TextBox Txt_FileName;
+        private System.Windows.Forms.Label Lb_File;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ FileSearch/ListScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSearch
+{
+    public partial class ListScreen : Form
+    {
+        public ListScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ListScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_FileName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(Txt_FileName.Text);
+            }
+            catch (Exception error)
+            {
+                Txt_FileName.Text = error.Message;
+            }
+        }
+
+        private void btn_SearchButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = @"C:\\";
+            openFileDialog.Title = "Procurar arquivos";
+            openFileDialog.DefaultExt = "txt";
+            openFileDialog.ShowDialog();
+            Txt_FileName.Text = openFileDialog.FileName;
+        }
+    }
+}
